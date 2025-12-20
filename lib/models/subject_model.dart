1932f1bfae459922930370e8599f0fd94a44f297
Nazmi Ramadani <ramadaninazmi47@gmail.com>
2025-12-20 17:51:35 +0700

    feat: add model database for subject

---
+++ lib/models/subject_model.dart
@@ -0,0 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'subject_model.g.dart';
+
+@JsonSerializable()
+class Subject {
+  final int id;
+  final String name;
+
+  Subject({required this.id, required this.name});
+
+  factory Subject.fromJson(Map<String, dynamic> json) => _$SubjectFromJson(json);
+  Map<String, dynamic> toJson() => _$SubjectToJson(this);
+}
